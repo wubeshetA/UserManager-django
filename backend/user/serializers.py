372b23f578a407bfdb6279613bc75a112c389d00
Wubeshet Yimam <w.yimam@alustudent.com>
2023-10-14 08:05:11 +0300

    Customize User model fields

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,13 +1,28 @@
-from django.urls import path, include
-# import the User model using the setting
-from django.contrib.auth import get_user_model 
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
 from django.contrib.auth.hashers import make_password 
-from rest_framework import routers, serializers, viewsets
-# Serializers define the API representation.
-
 
 User = get_user_model()
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password']
+        
+        
+    def __init__(self, *args, **kwargs):
+        super(UserSerializer, self).__init__(*args, **kwargs)
+
+        request = self.context.get('request', None)
+        if request and (request.method in ['POST', 'PUT']):
+            # Remove only 'id' field for POST and PUT requests
+            self.fields.pop('id', None)
+            
+    def to_representation(self, instance):
+        # Include 'id' field when data is being prepared for rendering
+            rep = super(UserSerializer, self).to_representation(instance)
+            rep['id'] = instance.id
+            return rep
+
+    def validate_password(self, value):
+        return make_password(value)
